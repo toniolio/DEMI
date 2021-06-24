644036887c45a9a08e8961817568cae05bf8a41c
Tony Ingram <tony.g.ingram@gmail.com>
2021-06-24 19:09:03 -0300

    setting this as default option

--- _Scripts/_settings.R
+++ _Scripts/_settings.R
@@ -11,7 +11,7 @@ drop_interpolated <- FALSE
 using_csd <- TRUE
 
 # Whether or not exclude imagery trials based on excess EMG activity
-exclude_bad_by_emg <- TRUE
+exclude_bad_by_emg <- FALSE
 
 # The frequency bins (in Hz) to use for wavelet-decomposition of the EEG signal
 wt_frequencies <- 1:40
